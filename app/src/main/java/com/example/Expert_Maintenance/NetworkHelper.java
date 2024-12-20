513b05b6ce3fe186376616ca8756199b8bdf9199
Ztat-Taher <ztattaher@gmail.com>
2024-12-20 17:16:49 +0100

    Add Android Expert Maintenance app

---
+++ app/src/main/java/com/example/Expert_Maintenance/NetworkHelper.java
@@ -0,0 +1,29 @@
+package com.example.Expert_Maintenance;
+
+import android.content.Context;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+public class NetworkHelper {
+
+    private static final String BASE_URL = "http://10.0.2.2/sync.php"; // Emulator uses this IP for localhost
+    private RequestQueue queue;
+
+    public NetworkHelper(Context context) {
+        queue = Volley.newRequestQueue(context);
+    }
+
+    public void fetchUsersFromServer(VolleyCallback callback) {
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, BASE_URL,
+                response -> callback.onSuccess(response),
+                error -> callback.onError(error.toString()));
+        queue.add(stringRequest);
+    }
+
+    public interface VolleyCallback {
+        void onSuccess(String response);
+        void onError(String error);
+    }
+}
